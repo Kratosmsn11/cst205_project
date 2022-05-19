1a93cf145bdfbbeb0649fd7b5b3aba185b7a5ba2
Rajx7 <92138297+Rajx7@users.noreply.github.com>
2022-05-18 23:29:28 -0700

    Implemented Routes
    Implemented Routes for uploading Image and For Accessing Image

--- app.py
+++ app.py
@@ -10,8 +10,46 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-@app.route("/")
-def home():
-	return render_template("Index.html")
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app.run() 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image Successfully Uploaded and Displayed Below')
+
+        img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # grayscale(img)
+        text = ocr_core(img)
+        # text = text.replace('\n', '<br>')
+        img.thumbnail((500,500))
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print(text)
+        if len(text)==0:
+            text = 'No text detected from image'
+        return render_template('index.html', filename=filename, text=text)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+app.run()
