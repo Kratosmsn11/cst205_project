a2f8f8508d8c72bf03775bf9180260967be12677
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2022-05-18 23:19:09 -0700

    Setting up Upload folder and allowed extensions
    Setting up Upload folder and allowed extensions..
    
    Vighnesh Prabhu

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask	, render_template
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
 @app.route("/")
 def home():
 	return render_template("Index.html")
