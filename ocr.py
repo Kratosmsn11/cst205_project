863e75505dbdbebf68ee905fe1c7849fafca8cb4
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2022-05-19 12:55:56 -0700

    Update ocr.py
    Updating ocr.py with few image pre processing steps

--- ocr.py
+++ ocr.py
@@ -23,8 +23,8 @@ def remove_noise(image):
 def thresholding(image):
     return cv2.threshold(image,0,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# img = get_grayscale(img)
-#img = thresholding(img)
-# img = remove_noise(img)
+img = get_grayscale(img)
+img = thresholding(img)
+img = remove_noise(img)
 
 print(ocr_core(img))
